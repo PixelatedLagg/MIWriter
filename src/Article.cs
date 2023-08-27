f365e9cc3a7ff0e4672eea0bfc04d52d41c10724
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2023-08-26 23:52:36 -0400

    + finished rendering

--- src/Article.cs
+++ src/Article.cs
@@ -4,20 +4,19 @@ namespace MIWriter
 {
     public class Article
     {
-        public string Title;
-        public string Url;
-        public List<Section> Sections;
+        public string Title = "";
+        public string Url = "";
+        public List<Section> Sections = new();
 
         public string Render()
         {
-            StringBuilder builder = new($"    \"{Url}\" : {{\n        \"title\" : \"{Title}\",        \"sections\" : [\n");
+            StringBuilder builder = new($"    \"{Url}\" : {{\n        \"title\" : \"{Title}\",\n        \"sections\" : [\n");
             foreach (Section section in Sections)
             {
-                builder.Append("            {\n");
-                builder.Append($"                \"text\" : \"{section.Text}\",\n                \"image\" : \"<div class=\\\"image\\\"><img src=\\\"{section.ImageUrl}\\\" alt=\\\"{section.ImageAlt}\\\">");
-
-                return builder.Append("\"\n                \"image\" : \"<div class=\\\"image\\\"><img src=\\\"url\\\" alt=\\\"alt\\\"><p>caption</p></div>\"").ToString();
+                builder.Append($"            {{\n                \"text\" : \"{section.Text}\",\n                \"image\" : \"<div class=\\\"image\\\"><img src=\\\"{section.ImageUrl}\\\" alt=\\\"{section.ImageAlt}\\\"><p>{section.Text}</p></div>\"\n            }},\n");
             }
+            builder.Length--; //remove last ','
+            return builder.Append("        ]\n    }").ToString();
         }
     }
 }
--- src/Section.cs
+++ src/Section.cs
@@ -1,20 +1,10 @@
-using System.Text;
-
 namespace MIWriter
 {
     public class Section
     {
-        public string Text;
-        public string ImageUrl;
-        public string ImageAlt;
-        public string ImageCaption;
-
-        public Section()
-        {
-            Text = "";
-            ImageUrl = "";
-            ImageAlt = "";
-            ImageCaption = "";
-        }
+        public string Text = "";
+        public string ImageUrl = "";
+        public string ImageAlt = "";
+        public string ImageCaption = "";
     }
 }
